6285e349300d3e804065eeeb195a6b5bea495565
Martin Nowosad <mrtnnwsd@gmail.com>
2021-04-17 02:21:43 +0200

    Lightweight Rust Client that serves as Redis-Subscriber for leromia.com

---
+++ src/data/redis_connector.rs
@@ -0,0 +1,42 @@
+use std::error::Error;
+
+use redis::{Commands, Connection, FromRedisValue, ToRedisArgs, PubSub};
+
+pub struct RedisConnector {
+    pub connection: redis::Connection,
+}
+
+fn connect(host: String) -> redis::RedisResult<Connection> {
+    let mut redis_host = String::from("redis://");
+    redis_host.push_str(&host);
+    print!("Connecting to {}", redis_host);
+    let client = &redis::Client::open(redis_host)?;
+
+    let con = client.get_connection()?;
+    Ok(con)
+}
+
+impl RedisConnector {
+    pub fn new<'a>(host: String) -> RedisConnector {
+        let connection = connect(host);
+        match connection {
+            Ok(result) => { RedisConnector { connection: result } }
+            Err(err) => {
+                panic!(err.to_string());
+            }
+        }
+    }
+
+    pub fn set<T: ToRedisArgs>(&mut self, key: &str, value: T) -> redis::RedisResult<()> {
+        let _: () = self.connection.set(key, value)?;
+        Ok(())
+    }
+
+    pub fn get<T: FromRedisValue>(&mut self, key: &str) -> redis::RedisResult<T> {
+        self.connection.get(key)
+    }
+
+    pub fn get_pub_sub(&mut self) -> PubSub {
+        self.connection.as_pubsub()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::sync::mpsc::channel;
+
+use redis::{RedisResult, ToRedisArgs};
+
+use crate::data::redis_connector::RedisConnector;
+
+mod data;
+
+fn main() {
+    let mut connector = RedisConnector::new(String::from("127.0.0.1:6379/0"));
+
+    let channel_name = "push_notifications";
+
+    let mut pub_sub = connector.get_pub_sub();
+    match pub_sub.subscribe(channel_name) {
+        Ok(()) => {
+            println!("got pub sub for to {}", channel_name);
+            loop {
+                let msg = pub_sub.get_message().unwrap();
+                let payload: String = msg.get_payload().unwrap();
+                println!("channel '{}': {}", msg.get_channel_name(), payload);
+            }
+        }
+        Err(e) => {
+            println!("Failed to Subscribe to {}: {}", channel_name, e)
+        }
+    }
+}
